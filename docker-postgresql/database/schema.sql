ea4d693f8c26a74bc1ad9df6097775cfe2f60e25
Олег Ігорович Хріпко <khripko.o@donnu.edu.ua>
2026-09-15 11:16:17 +0300

    feature: postgresql standalone image built with docker-compose

---
+++ docker-postgresql/database/schema.sql
@@ -0,0 +1,26 @@
+create table if not exists users (
+    id                  bigserial primary key,
+    telegram_id         bigint not null unique,
+    username            varchar(50),
+    created_at          timestamptz not null default current_timestamp
+);
+
+create table if not exists profiles (
+    id                  bigserial primary key,
+    user_id             bigint not null unique references users(id) on delete cascade,
+    name                varchar(50) not null,
+    faculty             varchar(100) not null,
+    degree              varchar(25) not null,
+    subject             varchar(50) not null,
+    goal                text not null,
+    is_active           boolean not null default true
+);
+
+create table if not exists likes (
+    id                  bigserial primary key,
+    liker_profile_id    bigint not null references profiles(id) on delete cascade,
+    liked_profile_id    bigint not null references profiles(id) on delete cascade,
+    created_at          timestamptz not null default current_timestamp,
+    constraint unique_like unique (liker_profile_id, liked_profile_id),
+    constraint no_self_like check (liker_profile_id <> liked_profile_id)
+);
